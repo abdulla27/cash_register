0b1127e707c9809a6234817b4f2671a2f2813a05
abdulla <abdulla27012002@gmail.com>
2023-01-08 16:18:15 +0530

    I am commiting my first code

---
+++ roman_converter.js
@@ -0,0 +1,30 @@
+function convert_to_roman(num){
+    var roman_to_num={
+        M:1000,
+        CM:900,
+        D:500,
+        CD:400,
+        C:100,
+        XC:90,
+        L:50,
+        XL:40,
+        X:10,
+        IX:9,
+        V:5,
+        IV:4,
+        I:1
+    };
+var roman="";
+
+for(var key in roman_to_num){
+    // console.log(key);
+    while(num>=roman_to_num[key]){
+        roman+=key;
+        num-=roman_to_num[key];
+        console.log(num);
+    }}
+    return roman;
+}
+
+
+convert_to_roman(3);
